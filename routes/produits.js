726af80e7872dda08102b10ec4c21e5728c63335
eommer <theocharlot@hotmail.fr>
2017-11-10 00:29:46 -0500

    Connexion avec BDD + GET / POST produits v1

--- routes/produits.js
+++ routes/produits.js
@@ -1,15 +1,42 @@
 var express = require("express");
 var router = express.Router();
 
+var mongoose = require("mongoose");
+mongoose.connect('mongodb://admin:1a2b3c@ds255265.mlab.com:55265/online-shop-db');
+
+var Product = mongoose.model('Product');
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+
 /* Get all products */
-router.get("", function(req, res) {
+router.get("/", function(req, res) {
   var category = req.param('category');
   var criteria = req.param('criteria');
-  if(category != null && criteria != null)
-    res.send('test api produits | '+ category +' | '+ criteria);
-  else {
-    res.send('test api produits');
-  }
+
+  Product.find({}, function(err, prods){
+    res.send(prods);
+  });
+});
+
+router.post("/", function(req, res) {
+  console.log(req.body);
+  var product = new Product();
+  product.id = req.body.id;
+  product.name = req.body.name;
+  product.price = req.body.price;
+  product.image = req.body.image;
+  product.category = req.body.category;
+  product.description = req.body.description;
+  product.features = req.body.features;
+
+  product.save(function(err) {
+            if (err)
+                res.send(err);
+
+            res.json(req.body);
+  });
+
+
 });
 
 
