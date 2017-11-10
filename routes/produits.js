a6730501741bb27971992081855d792a7530a43c
eommer <theocharlot@hotmail.fr>
2017-11-10 13:23:01 -0500

    Get /api/products

--- routes/produits.js
+++ routes/produits.js
@@ -10,12 +10,61 @@ var jsonParser = bodyParser.json();
 
 /* Get all products */
 router.get("/", function(req, res) {
-  var category = req.param('category');
-  var criteria = req.param('criteria');
+  var cat = req.param('category');
+  var crit = req.param('criteria');
+  var products = [];
 
-  Product.find({}, function(err, prods){
-    res.send(prods);
-  });
+  /* FIND */
+  /* Find all with given category */
+  if((cat != null && cat != "")){
+    Product.find({ category : cat}, function(err, prods){
+      /* CRITERIA */
+      if((crit != null && crit != "")){
+        function sortJson(a,b){
+          if(crit == "price-asc"){ return a.price > b.price? 1 : -1; }
+          else if(crit == "price-dsc"){ return a.price > b.price? -1 : 1; }
+          else if(crit == "alpha-asc"){
+            let nameLowerCaseA = a.name.toLowerCase();
+            let nameLowerCaseB = b.name.toLowerCase();
+            return nameLowerCaseA > nameLowerCaseB? 1 : -1;}
+          else if(crit == "alpha-dsc"){
+            let nameLowerCaseA = a.name.toLowerCase();
+            let nameLowerCaseB = b.name.toLowerCase();
+            return nameLowerCaseA > nameLowerCaseB? -1 : 1;}
+        }
+        prods.sort(sortJson);
+      }
+
+      res.json(prods);
+    });
+  }
+  /* Find all */
+  else{
+    Product.find({}, function(err, prods){
+      /* CRITERIA */
+      if((crit != null && crit != "")){
+        function sortJson(a,b){
+          if(crit == "price-asc"){ return a.price > b.price? 1 : -1; }
+          else if(crit == "price-dsc"){ return a.price > b.price? -1 : 1; }
+          else if(crit == "alpha-asc"){
+            let nameLowerCaseA = a.name.toLowerCase();
+            let nameLowerCaseB = b.name.toLowerCase();
+            return nameLowerCaseA > nameLowerCaseB? 1 : -1;}
+          else if(crit == "alpha-dsc"){
+            let nameLowerCaseA = a.name.toLowerCase();
+            let nameLowerCaseB = b.name.toLowerCase();
+            return nameLowerCaseA > nameLowerCaseB? -1 : 1;}
+        }
+        prods.sort(sortJson);
+      }
+
+      res.json(prods);
+    });
+  }
+
+
+
+
 });
 
 router.post("/", function(req, res) {
