db64d965d70c2a77ce5d5cd16d7d8bddd0a24f5b
eommer <theocharlot@hotmail.fr>
2017-11-15 12:44:58 -0500

    Product add to shopping-cart (NON TERMINÉ)

--- public/assets/scripts/script-product.js
+++ public/assets/scripts/script-product.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
 
   var getProductRequest = "http://localhost:8000/api/products/"+id;
 
-  console.log("|||||||||||||| " +getProductRequest);
+  console.log("|REQUEST GET PRODUCT| " +getProductRequest);
 
   //Rempli la page html avec les informations dans le fichier Json products.json
   $.getJSON( getProductRequest, function( data ) {
@@ -30,17 +30,20 @@ $(document).ready(function() {
 
               $('#product-price').html(price + "$");
 
-              ////// TODO
-              /*
+              ////// TODO récupère quantité du panier
+
               //Rempli la quantité si ce produit est déjà dans le panier
-              var productAct = JSON.parse(localStorage.getItem(actualProductId))
-              if(productAct != null){
-                  $('.form-control').attr('value', productAct.quantity);
-              }
-              isFound = true;
-              */
+              var getQuantityRequest = "http://localhost:8000/api/shopping-cart/"+id;
+              console.log("|REQUEST GET QUANITY| " +getProductRequest);
+              $.getJSON( getQuantityRequest, function( data ) {
+                //Si ce produit est déja dans le panier
+                if(data != null && data != "" && data.length != 0){
+                  $('.form-control').attr('value', data[0].quantity);
+                }
+              });
+
           }
-      })
+      });
 
       // Si l'élément n'a pas été trouvé dans le fichier jsons
       //(<=> l'id envoyé dans l'url ne correspond à aucun produit présent dans le fichier json)
@@ -52,36 +55,48 @@ $(document).ready(function() {
   });
 
 //////// TODO
-/*
+
   //Gestion du clic sur le bouton
   $('#add-to-cart-form').submit(function(e){
       //Évite de recharger la page
       e.preventDefault();
 
       if($('.form-control').val() > 0){
-          //Stockage dans le local storage
-          localStorage.setItem(actualProduct.id, JSON.stringify({id : actualProduct.id, name : actualProduct.name, price : actualProduct.price, quantity : $('.form-control').val()}));
 
-          //To calculate the quantity
-          var totalQuantity = 0;
-          $.each(localStorage, function(index, value){
-              if((index != -1) && (index != -2)){
-                  let product = JSON.parse(localStorage.getItem(index));
-                  console.log("actual product : " + product.name);
-                  totalQuantity = parseInt(totalQuantity) + parseInt(product.quantity);
-              }
+        var totalQuantity = 0;
+        var quantity = $('.form-control').val();
+
+        /* IF NOT ALREADY IN CART */
+        jQuery.ajax({
+          url : "http://localhost:8000/api/shopping-cart/",
+          type : "POST",
+          data : JSON.stringify({id : id, quantity : quantity}),
+          contentType : "application/json"
+        });
+
+        ///// TODO IF ALREADY IN CART
+
+
+        /* Calculates the quantity of items in the shopping-cart */
+        var totalQuantityRequest = "http://localhost:8000/api/shopping-cart/";
+        $.getJSON( totalQuantityRequest , function( data ) {
+          $.each( data, function( key, val ) {
+            totalQuantity += val.quantity;
           });
-          $('span.count').show();
-          $('span.count').html(totalQuantity);
+        });
 
-          //alert('Added to order : \n' + actualProduct.name + '\n With the quantity : ' + $('.form-control').val());
-          //console.log(lstShopProducts);
-          $('#dialog').slideUp( 300 ).delay( 0 ).fadeIn( 200 );
-          $('#dialog').slideDown( 300 ).delay( 4500 ).fadeOut( 300 );
+
+        $('span.count').show();
+        $('span.count').html(totalQuantity);
+
+        //alert('Added to order : \n' + actualProduct.name + '\n With the quantity : ' + $('.form-control').val());
+        //console.log(lstShopProducts);
+        $('#dialog').slideUp( 300 ).delay( 0 ).fadeIn( 200 );
+        $('#dialog').slideDown( 300 ).delay( 4500 ).fadeOut( 300 );
       }
       else{
           alert("Attention la quantité doit être supérieure à 0");
       }
-  });*/
+  });
 
 });
