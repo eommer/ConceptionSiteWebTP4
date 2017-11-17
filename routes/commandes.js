98c46903b1522e2da6a29f01132341ff89f5e35a
Azzorg <c.goupil72@gmail.com>
2017-11-17 17:54:54 -0500

    Serveur : commandes => Récupérer une commande en fonction de son id

--- routes/commandes.js
+++ routes/commandes.js
@@ -11,16 +11,35 @@ var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
 
 /** Récupère toutes les commandes présentes dans la base de données */
-router.get("/", function(req, res) {
-  Order.find(function(err, orders){
+router.get("/", function (req, res) {
+  Order.find(function (err, orders) {
+    if (err) throw err;
+
     console.log(orders);
     let lstOrders = [];
-    if(orders.length > 0){
+    if (orders.length > 0) {
       lstOrders = orders;
     }
-    res.status(200);
+    res.status(200);    //Code : 200(OK)
     res.json(lstOrders);
   })
+});
+
+
+/** Récupère la commande avec l'id mis en paramètre */
+router.get("/:id", function (req, res) {
+  Order.find({ id: req.params.id.toString() }, function (err, orders) {
+    if (err) throw err;
+
+    if(validator.isEmpty(orders.toString())){
+      res.status(404)   //Code : 404(Order not found)
+      res.send("Order not found");
+    }
+    else{
+      res.status(200)   //Code : 200(OK)
+      res.json(orders);
+    }
+  });
 });
 
 
