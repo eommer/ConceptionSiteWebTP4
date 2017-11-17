68bd93f89f3cd808451c8f0f4740081a1dd53de5
Azzorg <c.goupil72@gmail.com>
2017-11-17 15:52:46 -0500

    Client : script-shopping-cart => Récupération des produits du panier + récupération des informations les concernant + affichage des produits présents dans le panier

--- public/assets/scripts/script-shopping-cart.js
+++ public/assets/scripts/script-shopping-cart.js
@@ -1,208 +1,219 @@
 
-$(document).ready(function() {
+$(document).ready(function () {
 
   var lstProductsSorted = new Array();
-  var getPanierRequest = "http://localhost:8000/api/shopping-cart/";
-  var getProductsRequest = "http://localhost:8000/api/products/";
+  var getPanierRequest = "http://localhost:8000/api/shopping-cart";
+  var getProductsRequest = "http://localhost:8000/api/products";
   var items = new Array();
 
-    addItemsToHtmlShopping();
+  /** Récupère tous les produits du panier */
+  getItemsFromShoppingCart();
 
-    /* Ajout des élements au panier dans le html */
-    function addItemsToHtmlShopping(){
+  /** Get all items from the shopping-cart by REST request */
+  function getItemsFromShoppingCart() {
+    $.getJSON(getPanierRequest, function (data) {
+      console.log("data : " + data);
+      items = data;
+      nbProducts = items.length;
+      console.log("nombre product : " + nbProducts);
+    }).done(function () {
+      addItemsToHtmlShopping();
+    });
+  }
 
-      /* Récupère tous les produits du panier */
-      getItemsFromShoppingCart();
+  /** Ajout des élements au panier dans le html */
+  async function addItemsToHtmlShopping() {
 
-      /* Récupération des infomations liées aux produits présents dans le panier */ 
+    /* Si le panier est vide */
+    if (items.length == 0) {
+      $("#main-cart").empty();
+      $("#main-cart").append("<h1 id=\"shop-title\">Panier</h1><p id=\"emptyCart\">Aucun produit dans le panier.</p>");
+
+      //Cache le compte du panier
+      $('span.count').hide();
+    }
+    else {
+      /** Récupération des infomations liées aux produits présents dans le panier */
       lstProductsSorted = [];
-      
-      $.each(items, function(index, value){
-        var prod = JSON.parse(items.getItem(index));
-        var produc;
+      var allMyPromises = new Promise(function (resolve) {
+        var productPromises = items.map(function (value, index) {
+          return new Promise(function (ar) {
+            var produc;
+            console.log("id : " + value.idProduct);
+            var getProductRequest = getProductsRequest + "/" + value.idProduct;
 
-        let getProductRequest = getProductsRequest + prod.id;
-
-        $.getJSON( getProductRequest, function( data ) {
-          produc = JSON.parse(data);
-        }).done(function(){
-          lstProductsSorted.push({"id": produc.id, "name" : produc.name, "price": produc.price, "quantity": prod.quantity});
+            $.getJSON(getProductRequest, function (data) {
+              produc = data[0];
+              console.log(produc);
+            }).done(function () {
+              ar({ "id": produc.id, "name": produc.name, "price": produc.price, "quantity": value.quantity });
+              lstProductsSorted.push({ "id": produc.id, "name": produc.name, "price": produc.price, "quantity": value.quantity });
+              console.log("Nombre d'élément dans le panier : " + lstProductsSorted.length);
+            });
+          });
         });
+        Promise.all(productPromises).then(function (values) {
+          resolve(values);
+        })
       });
 
+      lstProductsSorted = await allMyPromises;
+      console.log(lstProductsSorted);
+      console.log("Traitemnet done");
       /* Calcul du prix total du panier */
       calculTotalPrice();
       var totalPriceStr = (((totalPrice).toFixed(2)).toString()).split(".")[0].toString() + "," + (((totalPrice).toFixed(2)).toString()).split(".")[1].toString();
 
-      /* Si le panier est vide */
-      if(items.length == 0){
-        $("#main-cart").empty();
-        $("#main-cart").append("<h1 id=\"shop-title\">Panier</h1><p id=\"emptyCart\">Aucun produit dans le panier.</p>");
+      $("#main-cart").empty();
+      $("#main-cart").append("<h1 id=\"shop-title\">Panier</h1>\
+                              <table id=\"table-shop\">\
+                                <tr class=\"tr-border\">\
+                                  <th></th>\
+                                  <th>Produit</th>\
+                                  <th>Prix unitaire</th>\
+                                  <th>Quantité</th>\
+                                  <th>Prix</th>\
+                                </tr>");
 
-        //Cache le compte du panier
-        $('span.count').hide();
+
+
+      /* Tri de la liste des items du panier */
+      function sortJson(a, b) {
+        let nameLowerCaseA = a.name.toLowerCase();
+        let nameLowerCaseB = b.name.toLowerCase();
+        return nameLowerCaseA > nameLowerCaseB ? 1 : -1;
       }
-      else{
-        $("#main-cart").empty();
-        $("#main-cart").append ("<h1 id=\"shop-title\">Panier</h1>\
-                                <table id=\"table-shop\">\
-                                  <tr class=\"tr-border\">\
-                                    <th></th>\
-                                    <th>Produit</th>\
-                                    <th>Prix unitaire</th>\
-                                    <th>Quantité</th>\
-                                    <th>Prix</th>\
+      lstProductsSorted.sort(sortJson);
+
+      /** Liste triée ajoutée au html */
+      $.each(lstProductsSorted, function (key, product) {
+
+        let price = ((product.price).toString()).split(".")[0].toString() + "," + ((product.price).toString()).split(".")[1].toString();
+        let priceTotalProduct = (((product.price * product.quantity).toFixed(2)).toString()).split(".")[0].toString() + "," + (((product.price * product.quantity).toFixed(2)).toString()).split(".")[1].toString();
+
+        $("#table-shop").append("<tr>\
+                                    <td><button id=\"delete-"+ product.id + "\" class=\"remove-item-button\">x</button></td>\
+                                    <td><a href=\"product.html?id="+ product.id + "\">" + (product.name).toString() + "</a></td>\
+                                    <td >"+ price + "$</td>\
+                                    <td><button id=\"reduce-"+ product.id + "\" class=\"remove-quantity-button\">-</button><div class=\"quantity\">" + product.quantity + "</div><button id=\"add-" + product.id + "\" class=\"add-quantity-button\">+</button></td>\
+                                    <td class=\"price\">"+ priceTotalProduct + "$</td>\
                                   </tr>");
+        if (product.quantity == 1) {
+          $("#reduce-" + product.id).attr('disabled', 'disabled');
+        }
 
+      });
 
+      $("#table-shop").after("</table>\
+                              <p class=\"total-cart\" id=\"total-amount\">Total : <b>"+ totalPriceStr + "$</b></p>\
+                                <div class=\"final-buttons-cart\">\
+                                <div class=\"col2 cancel-button\">\
+                                <button id=\"remove-all-items-button\" class=\"standardButton\">Vider le panier</button>\
+                                </div>\
+                                <form action=\"/commande\" class=\"col2 confirm-button\">\
+                                  <input type=\"submit\" value=\"Commander\" class=\"standardButton\">\
+                                </form>\
+                              </div>");
 
-          /* Tri de la liste des items du panier */
-          function sortJson(a,b){
-              let nameLowerCaseA = a.name.toLowerCase();
-              let nameLowerCaseB = b.name.toLowerCase();
-              return nameLowerCaseA > nameLowerCaseB? 1 : -1;
-          }
-          lstProductsSorted.sort(sortJson);
+      /** OnClick des boutons de suppression d'un produit */
+      $(".remove-item-button").click(function () {
+        var indexToRemove;
+        indexToRemove = this.id.split("-")[1];
 
-          /* Liste triée ajoutée au html */
-          $.each(lstProductsSorted, function( key, product ){
-
-            let price = ((product.price).toString()).split(".")[0].toString() + "," + ((product.price).toString()).split(".")[1].toString();
-            let priceTotalProduct = (((product.price*product.quantity).toFixed(2)).toString()).split(".")[0].toString() + "," + (((product.price*product.quantity).toFixed(2)).toString()).split(".")[1].toString();
-
-            $("#table-shop").append("<tr>\
-                                      <td><button id=\"delete-"+product.id+"\" class=\"remove-item-button\">x</button></td>\
-                                      <td><a href=\"product.html?id="+product.id+"\">"+(product.name).toString()+"</a></td>\
-                                      <td >"+price+"$</td>\
-                                      <td><button id=\"reduce-"+product.id+"\" class=\"remove-quantity-button\">-</button><div class=\"quantity\">"+product.quantity+"</div><button id=\"add-"+product.id+"\" class=\"add-quantity-button\">+</button></td>\
-                                      <td class=\"price\">"+priceTotalProduct+"$</td>\
-                                    </tr>");
-            if(product.quantity == 1){
-              $("#reduce-" + product.id).attr('disabled','disabled');
-            }
-
-          });
-
-        $("#table-shop").after ("</table>\
-                                <p class=\"total-cart\" id=\"total-amount\">Total : <b>"+totalPriceStr+"$</b></p>\
-                                  <div class=\"final-buttons-cart\">\
-                                  <div class=\"col2 cancel-button\">\
-                                  <button id=\"remove-all-items-button\" class=\"standardButton\">Vider le panier</button>\
-                                  </div>\
-                                  <form action=\"/commande\" class=\"col2 confirm-button\">\
-                                    <input type=\"submit\" value=\"Commander\" class=\"standardButton\">\
-                                  </form>\
-                                </div>");
-
-        /* OnClick des boutons de suppression d'un produit */
-        $(".remove-item-button").click(function(){
-          var indexToRemove;
-          indexToRemove = this.id.split("-")[1];
-
-          var responseConfirm = confirm("Voulez vous supprimer ce produit du panier?");
-          if(responseConfirm == true){
-            localStorage.removeItem(indexToRemove);
-            addItemsToHtmlShopping();
-
-            //To calculate the quantity
-            var totalQuantity = 0;
-            $.each(localStorage, function(index, value){
-                if(index >= 0){
-                    let product = JSON.parse(localStorage.getItem(index));
-                    totalQuantity = parseInt(totalQuantity) + parseInt(product.quantity);
-                }
-            });
-
-            $('span.count').html(calculTotalQuantity);
-          }
-        });
-
-        /* Onclick des boutons de diminution de la quantité */
-        $(".remove-quantity-button").click(function(){
-          var indexToReduce;
-          indexToReduce = this.id.split("-")[1];
-          var productToReduce = JSON.parse(localStorage.getItem(indexToReduce));
-          if(productToReduce.quantity > 1){
-            productToReduce.quantity --;
-            localStorage.setItem(productToReduce.id, JSON.stringify(productToReduce));
-          }
-          addItemsToHtmlShopping();
-          if(productToReduce.quantity === 1){
-            $("#"+this.id).attr('disabled','disabled');
-          }
-          else{
-            $("#"+this.id).removeAttr('disabled');
-          }
-
-          //Changement du compte du panier
-          $('span.count').html(calculTotalQuantity);
-        });
-
-        /* Onclick des boutons d'augmentation de la quantité */
-        $(".add-quantity-button").click(function(){
-          var indexToIncrease;
-          indexToIncrease = this.id.split("-")[1];
-
-          var productToIncrease = JSON.parse(localStorage.getItem(indexToIncrease));
-          productToIncrease.quantity ++;
-          localStorage.setItem(productToIncrease.id, JSON.stringify(productToIncrease));
+        var responseConfirm = confirm("Voulez vous supprimer ce produit du panier?");
+        if (responseConfirm == true) {
+          localStorage.removeItem(indexToRemove);
           addItemsToHtmlShopping();
 
           //To calculate the quantity
           var totalQuantity = 0;
-          $.each(localStorage, function(index, value){
-              if((index != -1) && (index != -2)){
-                  let product = JSON.parse(localStorage.getItem(index));
-                  totalQuantity = parseInt(totalQuantity) + parseInt(product.quantity);
-              }
+          $.each(localStorage, function (index, value) {
+            if (index >= 0) {
+              let product = JSON.parse(localStorage.getItem(index));
+              totalQuantity = parseInt(totalQuantity) + parseInt(product.quantity);
+            }
           });
 
-          //Changement du compte du panier
           $('span.count').html(calculTotalQuantity);
-        });
+        }
+      });
 
-        /* OnClick du bouton de suppression du panier complet */
-        $("#remove-all-items-button").click(function(){
-          var responseConfirm = confirm("Voulez-vous supprimer tous les produits du panier ?");
-          if(responseConfirm == true){
-            //Id commandes est sauvegardé le localStorage à l'index -1
-            var numCommande = JSON.parse(localStorage.getItem(-1));
-            localStorage.clear();
-            localStorage.setItem(-1, JSON.stringify(numCommande));
-            $('span.count').hide();
-            addItemsToHtmlShopping();
+      /* Onclick des boutons de diminution de la quantité */
+      $(".remove-quantity-button").click(function () {
+        var indexToReduce;
+        indexToReduce = this.id.split("-")[1];
+        var productToReduce = JSON.parse(localStorage.getItem(indexToReduce));
+        if (productToReduce.quantity > 1) {
+          productToReduce.quantity--;
+          localStorage.setItem(productToReduce.id, JSON.stringify(productToReduce));
+        }
+        addItemsToHtmlShopping();
+        if (productToReduce.quantity === 1) {
+          $("#" + this.id).attr('disabled', 'disabled');
+        }
+        else {
+          $("#" + this.id).removeAttr('disabled');
+        }
+
+        //Changement du compte du panier
+        $('span.count').html(calculTotalQuantity);
+      });
+
+      /* Onclick des boutons d'augmentation de la quantité */
+      $(".add-quantity-button").click(function () {
+        var indexToIncrease;
+        indexToIncrease = this.id.split("-")[1];
+
+        var productToIncrease = JSON.parse(localStorage.getItem(indexToIncrease));
+        productToIncrease.quantity++;
+        localStorage.setItem(productToIncrease.id, JSON.stringify(productToIncrease));
+        addItemsToHtmlShopping();
+
+        //To calculate the quantity
+        var totalQuantity = 0;
+        $.each(localStorage, function (index, value) {
+          if ((index != -1) && (index != -2)) {
+            let product = JSON.parse(localStorage.getItem(index));
+            totalQuantity = parseInt(totalQuantity) + parseInt(product.quantity);
           }
         });
-      }
-    };
 
-    /* Get all items from the shopping-cart by REST request */
-    function getItemsFromShoppingCart(){
-      $.getJSON( getPanierRequest , function( data ) {
-        items = data;
-        nbProducts = items.length;
-        console.log("nombre product : " + nbProducts);
-      }).done(function(){
-        getItemsInformationFromJson();
+        //Changement du compte du panier
+        $('span.count').html(calculTotalQuantity);
+      });
+
+      /* OnClick du bouton de suppression du panier complet */
+      $("#remove-all-items-button").click(function () {
+        var responseConfirm = confirm("Voulez-vous supprimer tous les produits du panier ?");
+        if (responseConfirm == true) {
+          //Id commandes est sauvegardé le localStorage à l'index -1
+          var numCommande = JSON.parse(localStorage.getItem(-1));
+          localStorage.clear();
+          localStorage.setItem(-1, JSON.stringify(numCommande));
+          $('span.count').hide();
+          addItemsToHtmlShopping();
+        }
       });
     }
+  };
 
-    /* Calcule le prix total du panier */
-    function calculTotalPrice(){
-      totalPrice = 0;
-      $.each(lstProductsSorted, function(index, value){
-        var product = JSON.parse(lstProductsSorted.getItem(index));
-        totalPrice += (product.price*product.quantity);
-      });
-    };
 
-    /* Calcule la quantité de produit présent dans le panier */ 
-    function calculTotalQuantity(){
-      var totalQuantity = 0;
-      $.each(lstProductsSorted, function(index, value){
-        let product = JSON.parse(lstProductsSorted.getItem(index));
-        totalQuantity = parseInt(totalQuantity) + parseInt(product.quantity);
-      });
-      return totalQuantity;
-    };
+  /* Calcule le prix total du panier */
+  function calculTotalPrice() {
+    totalPrice = 0;
+    $.each(lstProductsSorted, function (index, value) {
+      var product = lstProductsSorted[index];
+      totalPrice += (product.price * product.quantity);
+    });
+  };
+
+  /* Calcule la quantité de produit présent dans le panier */
+  function calculTotalQuantity() {
+    var totalQuantity = 0;
+    $.each(lstProductsSorted, function (index, value) {
+      let product = lstProductsSorted[index];
+      totalQuantity = parseInt(totalQuantity) + parseInt(product.quantity);
+    });
+    return totalQuantity;
+  };
 });
