c24f41e626df0cde907daacee7bebbce6b519faa
Azzorg <c.goupil72@gmail.com>
2017-11-17 17:48:17 -0500

    Serveur : commandes.js => Récupérer toutes les commandes

--- routes/commandes.js
+++ routes/commandes.js
@@ -1,8 +1,26 @@
 var express = require("express");
 var router = express.Router();
 
+var validator = require('validator');
+
+var mongoose = require("mongoose");
+mongoose.connect('mongodb://admin:1a2b3c@ds255265.mlab.com:55265/online-shop-db');
+
+var Order = mongoose.model('Order');
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+
+/** Récupère toutes les commandes présentes dans la base de données */
 router.get("/", function(req, res) {
-  res.send('test api commandes');
+  Order.find(function(err, orders){
+    console.log(orders);
+    let lstOrders = [];
+    if(orders.length > 0){
+      lstOrders = orders;
+    }
+    res.status(200);
+    res.json(lstOrders);
+  })
 });
 
 
