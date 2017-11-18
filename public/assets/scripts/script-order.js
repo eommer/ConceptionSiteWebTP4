9bbf89c0f52a5c6541b375764fd9228a4a107f1d
Azzorg <c.goupil72@gmail.com>
2017-11-17 20:58:16 -0500

    Client : script-order => Pb avec l'événement submit + mais enregistrement dans la base de données OK

--- public/assets/scripts/script-order.js
+++ public/assets/scripts/script-order.js
@@ -1,63 +1,135 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  var orderRequest = "http://localhost:8000/api/order/";
+  var shoppingCartRequest = "http://localhost:8000/api/shopping-cart/";
+  var orders = new Array();
+  var items = new Array();
+  var order;
+
+
   var validator = $("#order-form").validate({
-    rules : {
+    rules: {
       firstname: {
-        required : true,
-        minlength : 2
+        required: true,
+        minlength: 2
       },
       lastname: {
-        required : true,
-        minlength : 2
+        required: true,
+        minlength: 2
       },
       email: {
-        required : true,
-        email : true
+        required: true,
+        email: true
       },
       phone: {
-        required : true,
-        phoneUS : true
+        required: true,
+        phoneUS: true
       },
       creditcard: {
-        required : true,
-        creditcard : true
+        required: true,
+        creditcard: true
       },
       creditcardexpiry: {
-        required : true,
-        creditcardexpiry : true
+        required: true,
+        creditcardexpiry: true
       },
     },
-    messages : {
+    messages: {
     },
-    submitHandler: function() {
+    submitHandler: function () {
       console.log("onClickValidate");
-      var nbCommande = JSON.parse(localStorage.getItem(-1));
 
-      localStorage.clear();
+      $.getJSON(orderRequest, function (data) {
+        orders = data;
+        let nbOrders = orders.length;
+        console.log("nombre de commande : " + nbOrders);
+      }).done(function () {
+        $.getJSON(shoppingCartRequest, function (data) {
+          items = data;
+        }).done(function () {
+          //Pas encore de commande dans la base de données
+          if (orders.length == 0) {
+            order = JSON.stringify({ "id": 0, "firstName": $("#first-name").val(), "lastName": $("#last-name").val(), "email": $("#email").val(), "phone": $("#phone").val(), "products": items });
+          }
+          else {
+            order = JSON.stringify({ "id": parseInt(orders[orders.length-1].id) + 1, "firstName": $("#first-name").val(), "lastName": $("#last-name").val(), "email": $("#email").val(), "phone": $("#phone").val(), "products": items });
+          }
 
-      console.log("firstname value : " + $("#first-name").val());
-      //Sauvegarde du nom et du prénom de l'utilisateur en localStorage
-      localStorage.setItem(-2, JSON.stringify({firstname : $("#first-name").val(), lastname : $("#last-name").val()}));
+          jQuery.ajax({
+            url: orderRequest,
+            type: "POST",
+            data: order,
+            contentType: "application/json",
+            complete: function (res) {
+              console.log("res : " + res.status);
 
-      //Sauvegarde du numéro de commande dans le localStorage
-      if(nbCommande != null){
-        nbCommande ++;
-        localStorage.setItem(-1, JSON.stringify(nbCommande));
-      }
-      else{
-        nbCommande = 1
-        localStorage.setItem(-1, JSON.stringify(nbCommande));
-      }
+              if(res.status == 201){
+                //$("#order-form").ajaxSubmit();
+                submitHandler();
+                $("#order-form").submit();
+              }
+              else{
+                alert("Erreur dans l'enregistrement de votre commande : " + res);
+              }
+            }
+          });
+      });
+    });
 
-      console.log("nbCommande : " + nbCommande);
-
-      $("#order-form").ajaxSubmit();
+      //getOrders();
     }
   });
 
-  $.validator.addMethod("creditcardexpiry", function(value, element) {
+  $.validator.addMethod("creditcardexpiry", function (value, element) {
     // allow any non-whitespace characters as the host part
-    return this.optional( element ) || /^(0[1-9]|1[0-2])\/\d{2}$/.test( value );
+    return this.optional(element) || /^(0[1-9]|1[0-2])\/\d{2}$/.test(value);
   }, "La date d\'expiration de votre carte de crédit est invalide.");
 
   validator.form();
 });
+
+
+/** Permet de récupérer la liste des commandes de la base de données */
+/*function getOrders() {
+  $.getJSON(orderRequest, function (data) {
+    orders = data;
+    let nbOrders = items.length;
+    console.log("nombre de commande : " + nbOrders);
+  }).done(function () {
+    getItemsFromOrder();
+  });
+}*/
+
+
+/** Récupère les produits enregistrés dans le panier */
+/*function getItemsFromOrder() {
+  $.getJSON(shoppingCartRequest, function (data) {
+    items = data;
+  }).done(function () {
+    //Pas encore de commande dans la base de données
+    if (orders.length == 0) {
+      order = JSON.stringify({ "id": 0, "firstName": $("#first-name").val(), "lastName": $("#last-name").val(), "email": $("#email").val(), "phone": $("#phone").val(), "products": items });
+    }
+    else {
+      order = JSON.stringify({ "id": parseInt(orders[orders.length].id) + 1, "firstName": $("#first-name").val(), "lastName": $("#last-name").val(), "email": $("#email").val(), "phone": $("#phone").val(), "products": items });
+    }
+
+    //Envoie au serveur de la commande pour sauvegarder dans la base de données
+    postOrder();
+
+  });
+}*/
+
+/** Envoie au serveur de la commande pour la sauvegarder dans la base de données */
+/*function postOrder() {
+  jQuery.ajax({
+    url: orderRequest,
+    type: "POST",
+    data: order,
+    contentType: "application/json",
+    complete: function (res) {
+      console.log("res : " + res.status);
+      
+      $("#order-form").ajaxSubmit();
+    }
+  });
+}*/
