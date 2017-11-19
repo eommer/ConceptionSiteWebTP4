f1690ff7afc487c37eb05a1be9231b74012eb0e1
Azzorg <c.goupil72@gmail.com>
2017-11-18 23:34:53 -0500

    Client : script-confirmation : OK affichage des informations

--- public/assets/scripts/script-confirmation.js
+++ public/assets/scripts/script-confirmation.js
@@ -1,7 +1,30 @@
-$(document).ready(function() {
-    var numCommande = JSON.parse(localStorage.getItem(-1));
-    var user = JSON.parse(localStorage.getItem(-2));
-    console.log("num de commande : " + numCommande);
-    $("#confirmation-number").html(numCommande);
-    $("#name").html(user.firstname + " " + user.lastname);
+var orderRequest;
+
+$(document).ready(function () {
+    var order;
+    var id;
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('=');
+    id = sURLVariables[1];
+
+    orderRequest = "http://localhost:8000/api/order/" + id;
+
+    console.log("|REQUEST| : " + orderRequest);
+
+    getOrder();
 });
+
+function getOrder() {
+    console.log(orderRequest);
+
+    $.getJSON(orderRequest, function (data) {
+        order = data[0];
+        console.log(order);
+    }).done(function () {
+        var numCommande = " " + order.id;
+        var user = " " + order.firstName + " " + order.lastName;
+
+        $("#confirmation-number").html(numCommande);
+        $("#name").html(user);
+    });
+}
